d5232cddde31beea1a9e9f1de2f000a8b97b3458
cd <viktor@Viktors-MacBook-Air.local>
2020-06-03 22:04:32 +0200

    Initial savings

---
+++ Kalkulacka/ViewController.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Didtaptitle: UILabel!
+        
+    var num : Int = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    func print(_: Inum)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+  
+    
+}
